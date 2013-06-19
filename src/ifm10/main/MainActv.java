1fb5f6dd230b1ee494a37feef3d5560f9fdf5d08
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-06-19 11:06:00 +0900

    m
    v-2.0
    ---
    1. Setup: libs fils
    	==> Done

---
+++ src/ifm10/main/MainActv.java
@@ -0,0 +1,39 @@
+package ifm10.main;
+
+
+import java.io.File;
+
+import org.apache.commons.lang.StringUtils;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActv extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.actv_main);
+		
+		// Log
+		Log.d("[" + "MainActv.java : "
+				+ +Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "YES");
+		
+		// Log
+		Log.d("[" + "MainActv.java : "
+				+ +Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]",
+			"message" + StringUtils.join(new String[]{"/data/data/ifm9.main/databases", "trillion"}, File.separator));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_actv, menu);
+		return true;
+	}
+
+}
